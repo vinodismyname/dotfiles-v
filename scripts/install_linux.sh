aa23a86edb36b61e47dee8ae34ed893513984068
Vinod Durairaj <vinodismyname@msn.com>
2025-02-12 22:07:08 +0000

    env-update

--- scripts/install_linux.sh
+++ scripts/install_linux.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-# ─────────────────────────────────────────────────────────────────────────────
-# scripts/install_linux.sh - Additional Linux-specific setup
-# ─────────────────────────────────────────────────────────────────────────────
+# scripts/install_linux.sh - Additional Linux-specific setup for Dotfiles
 set -euo pipefail
 
 DRY_RUN="${1:-false}"
@@ -10,22 +8,22 @@ source "$( dirname "${BASH_SOURCE[0]}" )/../scripts/ui_components.sh"
 
 subheading "Linux-specific Setup"
 
-
-
 info_msg "Updating Yum Package Manager..."
 if command -v yum &>/dev/null; then
-  run_with_spinner "yum update..."  "sleep 1"
+  run_with_spinner "yum update..." "sleep 1"
   if [ "$DRY_RUN" = false ]; then
     sudo yum update -y &>/dev/null
   else
     warn_msg "[DRY_RUN] sudo yum update -y"
   fi
   success_msg "yum updated"
+else
+  warn_msg "yum not found; skipping system update."
 fi
 
 echo
 
-info_msg "Installing Yum Default Packages..."
+info_msg "Installing essential packages via yum..."
 PACKAGES=(lshw pciutils)
 for pkg in "${PACKAGES[@]}"; do
   if [ "$DRY_RUN" = false ]; then
@@ -33,8 +31,8 @@ for pkg in "${PACKAGES[@]}"; do
       sudo yum install -y "$pkg" &>/dev/null
     fi
   else
-    warn_msg "[DRY_RUN] installing $pkg"
+    warn_msg "[DRY_RUN] Would install $pkg"
   fi
 done
 
-success_msg "Installed Yum Packages"
+success_msg "Finished installing Linux prerequisites"
